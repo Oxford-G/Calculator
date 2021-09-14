9590b2156327c765b7ac03c946b301d842412398
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-09-14 09:31:24 +0100

    modified operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,35 +1,38 @@
 import Big from 'big.js';
 
-const OPERATORS = ['+', '*', '/', '-'];
+function operate(numberOne, numberTwo, operation) {
+  let result;
+  if (!numberOne || !numberTwo || !operation) return '0';
+  if (/[a-zA-Z]/.test(numberOne) && operation) {
+    return 'Invalid Number';
+  }
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
 
-const operate = (numberOne, numberTwo, operation) => {
-  const calculate = (num1, num2, op) => {
-    switch (op) {
-      case '+': return num1.plus(num2).toFixed();
-      case '*': return num1.times(num2).toFixed();
-      case '/': return numberTwo !== '0' ? num1.div(num2).toFixed() : 'inf';
-      case '-': return num1.minus(num2).toFixed();
-      default: return null;
-    }
-  };
-
-  const checkOperator = (op) => {
-    if (numberTwo.lastIndexOf(op) !== -1) {
-      const num1 = new Big(numberTwo.slice(0, numberTwo.indexOf(op)));
-      const num2 = new Big(numberTwo.slice(numberTwo.indexOf(op) + 1));
-      return calculate(num1, num2, op);
-    }
-    return null;
-  };
-
-  for (let i = 0; i < OPERATORS.length; i += 1) {
-    const result = checkOperator(OPERATORS[i]);
-    if (result) {
-      return result + (operation || '');
-    }
+  switch (operation) {
+    case '+':
+      result = num1.plus(num2);
+      break;
+    case '-':
+      result = num1.minus(num2);
+      break;
+    case 'x':
+      result = num1.times(num2);
+      break;
+    case '÷':
+      result = num1.div(num2);
+      break;
+    case '%':
+      result = num1.times(num2).div(100);
+      break;
+    case '+/-':
+      result = num1.times(-1);
+      break;
+    default:
+      result = 0;
   }
 
-  return null;
-};
+  return result.toString();
+}
 
-export { operate, OPERATORS };
+export default operate;
